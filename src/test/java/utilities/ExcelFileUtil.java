b9fb93eb5d4e32b3f67fae845454e2ee2a50ba1e
vikas <vikas@LAPTOP-7LJFOLQK.lan>
2024-10-17 01:53:08 +0800

    First Commit

---
+++ src/test/java/utilities/ExcelFileUtil.java
@@ -0,0 +1,88 @@
+ package utilities;
+ 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelFileUtil {
+XSSFWorkbook wb;
+//write constructor for reading excel path
+public ExcelFileUtil(String Excelpath)throws Throwable
+{
+	FileInputStream fi = new FileInputStream(Excelpath);
+	wb = new XSSFWorkbook(fi);
+}
+//method for counting no of rows in a sheet
+public int rowCount(String sheetName)
+{
+	return wb.getSheet(sheetName).getLastRowNum();
+}
+//method for reading cell data
+public String getCellData(String sheetName,int row,int column)
+{
+	String data="";
+	if(wb.getSheet(sheetName).getRow(row).getCell(column).getCellType()==CellType.NUMERIC)
+	{
+		int celldata =(int) wb.getSheet(sheetName).getRow(row).getCell(column).getNumericCellValue();
+		data =String.valueOf(celldata);
+	}
+	else
+	{
+		data =wb.getSheet(sheetName).getRow(row).getCell(column).getStringCellValue();
+	}
+	return data;
+}
+//method for writeing results into new wb
+public void setCellData(String sheetName,int row,int column,String status,String WriteExcel)throws Throwable
+{
+	//get sheet from wb
+	XSSFSheet ws = wb.getSheet(sheetName);
+	//get row from sheet
+	XSSFRow rowNum =ws.getRow(row);
+	//create cell inrow
+	XSSFCell cell =rowNum.createCell(column);
+	cell.setCellValue(status);
+	if(status.equalsIgnoreCase("Pass"))
+	{
+		XSSFCellStyle style = wb.createCellStyle();
+		XSSFFont font = wb.createFont();
+		font.setColor(IndexedColors.GREEN.getIndex());
+		font.setBold(true);
+		style.setFont(font);
+		rowNum.getCell(column).setCellStyle(style);
+	}
+	else if(status.equalsIgnoreCase("Fail"))
+	{
+		XSSFCellStyle style = wb.createCellStyle();
+		XSSFFont font = wb.createFont();
+		font.setColor(IndexedColors.RED.getIndex());
+		font.setBold(true);
+		style.setFont(font);
+		rowNum.getCell(column).setCellStyle(style);
+	}
+	else if(status.equalsIgnoreCase("Blocked"))
+	{
+		XSSFCellStyle style = wb.createCellStyle();
+		XSSFFont font = wb.createFont();
+		font.setColor(IndexedColors.BLUE.getIndex());
+		font.setBold(true);
+		style.setFont(font);
+		rowNum.getCell(column).setCellStyle(style);
+	}
+	FileOutputStream fo = new FileOutputStream(WriteExcel);
+	wb.write(fo);
+}
+}
+
+   
+
+
+
